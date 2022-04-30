67d4253a2f34636e5cfca99dd13ae25daf4273ec
floduat <floduatgamer@gmail.com>
2022-04-30 15:26:23 +0300

    fix weather for aio 3.0

--- app/handlers/user/weather.py
+++ app/handlers/user/weather.py
@@ -3,10 +3,11 @@ from aiogram.types import Message
 import requests
 from app import dp
 from app.common import FMT
+from aiogram.dispatcher.filters import Command, CommandObject
+
 
 @dp.message(commands=['weather','wttr'])
-async def weather(message: Message, f: FMT, bot: Bot):
-    commaarg = message.get_args()
-    weather1 = requests.get(f"https://wttr.in/{commaarg}?format=3")
+async def weather(message: Message, f: FMT, bot: Bot, command: CommandObject):
+    weather1 = requests.get(f"https://wttr.in/{command.args}?format=3")
     weather_reply = weather1.text
     await bot.send_message(text=f"{weather_reply}", chat_id=message.from_user.id)
